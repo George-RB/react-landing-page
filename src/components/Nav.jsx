8bc4d7c935947f2f79b631df548ad3f4db127166
Heorhi Kartavenko <SocialImp@mail.ru>
2025-06-15 13:22:09 +0300

    :feat: new component Nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed ipsam
+        officia, quia minima corporis ullam, quae delectus quidem eos harum
+        dicta, id unde!
+      </p>
+    </>
+  );
+}
+
+export default Nav;
